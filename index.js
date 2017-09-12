ce711bdbc230def5ea7d59bda893cc3ded824ebc
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-09-11 18:36:10 -0700

    adds 'dirs' config option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path');
-module.exports = function(babel){
+module.exports = function(babel, opts){
   var t = babel.types;
+
   return {
     visitor: {
       ImportDeclaration: function(babelPath){
@@ -11,6 +12,13 @@ module.exports = function(babel){
         var depPath = source.value;
 
         if (depPath[0] !== '@') return;
+        if (opts && opts.dirs) {
+          var first = depPath.slice(1, depPath.indexOf('/'));
+          if (!first) return;
+          if (dirs.indexOf(first) === -1) {
+            return;
+          }
+        }
         depPath = depPath.slice(1);
 
         var absolutePath = path.join(process.cwd(), 'src', depPath);
